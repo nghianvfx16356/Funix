63e611da0cd6eb64f63c87b55147e910e36bb3c1
Nghia <nghiavfx16356@funix.edu.vn>
2022-07-05 21:43:24 +0700

    Update

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color = 'primary'>
+        <div className='container'>
+          <NavbarBand href='/'> Ristorante Con Fusion</NavbarBand>
+        </div>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
