581c92c4197844f60ef230500f328f059d4e97a5
Nghia <nghiavfx16356@funix.edu.vn>
2022-07-07 20:52:17 +0700

    Configuring React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color = 'primary'>
-        <div className='container'>
-          <NavbarBand href='/'> Ristorante Con Fusion</NavbarBand>
-        </div>
-      </Navbar>
-    </div>
-  );
+
+import { Navbar , NavbarBrand } from 'reactstrap';
+import { Component } from 'react';
+
+class App extends Component{
+  render(){
+    return (
+      <div className='App'>
+        <Navbar dark color='primary'>
+          <div className='container'>
+            <NavbarBrand href='/'>
+              Ristorante Con Fusion
+            </NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    )
+  }
 }
 
 export default App;
